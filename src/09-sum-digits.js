41837f0a069557e1f2821cfaae7c8593705fdc2f
Git-DmR <dm.r@mail.uk>
2021-04-09 14:47:07 +0300

    feat: 09-sum-digit solved

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -9,9 +9,10 @@
  * For 100, the result should be 1 (1 + 0 + 0 = 1)
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
+ * https://en.wikipedia.org/wiki/Digital_root
  */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  return n % 9 || (n && 9);
 }
 
 module.exports = getSumOfDigits;
