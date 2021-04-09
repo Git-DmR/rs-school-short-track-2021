c6259d2001c550c590f248c43f4ccfbe003c2d87
Git-DmR <dm.r@mail.uk>
2021-04-09 14:26:44 +0300

    feat: 11-encode-line solved

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,20 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let count = 1;
+  let result = '';
+  let i = 0;
+  while (i < str.length) {
+    while (str[i] === str[i + 1]) {
+      count++;
+      i++;
+    }
+    result += count > 1 ? `${count}${str[i]}` : str[i];
+    i++;
+    count = 1;
+  }
+  return result;
 }
 
 module.exports = encodeLine;
